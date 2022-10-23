cbbe942b8e94931ca04476eaea63e05ed75d723e
JusSix1 <patcharapolwor28112544@gmail.com>
2022-10-23 20:13:02 +0700

    final real real

--- backend/controller/bloodGroups/bloodgroups.go
+++ backend/controller/bloodGroups/bloodgroups.go
@@ -3,8 +3,9 @@ package controller
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"github.com/non-nattawut/patient-management-system/entity"
+
+	"github.com/gin-gonic/gin"
 )
 
 // GET /BloodGroups/:id
